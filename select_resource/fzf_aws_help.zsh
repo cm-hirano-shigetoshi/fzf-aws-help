3b113dad27b96e13dd7a83e59964411ce3388b9f
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-06-23 17:56:10 +0900

    pythonで処理するように変更

--- select_resource/fzf_aws_help.zsh
+++ select_resource/fzf_aws_help.zsh
@@ -6,29 +6,12 @@ function aws_resource_info {
   if [[ -z ${AWS_PROFILE} ]]; then
     export AWS_PROFILE=default
   fi
-  local cmd_file input_cmd
-  readonly local ORIG_CMD=$(python ${FZF_AWS_HELP_RESOURCE_HOME}/get_target_file_name.py $CURSOR "${BUFFER}")
-
-  cmd_file=${FZF_AWS_HELP_RESOURCE_HOME}/commands/input_${ORIG_CMD}
-  if [[ ! -e ${cmd_file} ]]; then
-    cmd_file=${cmd_file%_*}
-    if [[ ! -e ${cmd_file} ]]; then
-      cmd_file=${cmd_file%_*}
-      if [[ ! -e ${cmd_file} ]]; then
-        echo "Please create the command file: ${FZF_AWS_HELP_RESOURCE_HOME}/commands/input_${ORIG_CMD}" >&2
-        return 1
-      fi
-    fi
+  readonly local RESULT=$(python ${FZF_AWS_HELP_RESOURCE_HOME}/get_target_file_name.py $CURSOR "${BUFFER}")
+  if [[ -n ${RESULT} ]]; then
+    BUFFER=$(sed '1d' <<< ${RESULT})
+    CURSOR=$(head -n -1 <<< ${RESULT})
+    zle redisplay
   fi
-  input_cmd=$(cat ${cmd_file})
-
-  local result_file
-  result_file=${FZF_AWS_HELP_RESOURCE_RESULT_DIR}/${AWS_PROFILE}/result_${cmd_file##*/commands/input_}
-  mkdir -p $(dirname ${result_file})
-  if [[ ! -s ${result_file} ]]; then
-    bash -c "${input_cmd}" | sort > ${result_file}
-  fi
-  cat ${result_file} | fzf -m --reverse
 }
 zle -N aws_resource_info
 bindkey "^j" aws_resource_info
--- select_resource/get_target_file_name.py
+++ select_resource/get_target_file_name.py
@@ -1,7 +1,66 @@
+import os
 import sys
+import re
 import shlex
-buf = sys.argv[2]
-pos = int(sys.argv[1])
+import subprocess
+from subprocess import PIPE
+
+
+def select_resources(input_cmd):
+    proc = subprocess.run(
+        input_cmd + " | fzf -m --reverse", shell=True, stdout=PIPE)
+    stdout = re.sub(r'\s+$', '', proc.stdout.decode('utf8'))
+    return stdout
+
+
+def get_input_command(basename):
+    input_file = "{}/commands/input_{}".format(
+        os.environ.get("FZF_AWS_HELP_RESOURCE_HOME"), basename)
+    result_file = "{}/{}/result{}".format(
+        os.environ.get("FZF_AWS_HELP_RESOURCE_RESULT_DIR"),
+        os.environ.get("AWS_PROFILE", "default"), basename)
+    if not os.path.exists(input_file):
+        return ""
+    if not os.path.exists(result_file):
+        with open(input_file, "r") as f:
+            cmd = "\n".join(f.readlines())
+            cmd = re.sub(r'\s+$', '', cmd)
+        with open(result_file, "w") as f:
+            proc = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+            f.write(re.sub(r'\s+$', '', proc.stdout.decode('utf8')))
+    return "cat " + result_file
+
+
+def get_input_cmd(service, subcmd, method):
+    input_cmd = ""
+    basename = "{}_{}_{}".format(method, service, subcmd)
+    while basename.find("_") >= 0:
+        input_cmd = get_input_command(basename)
+        if input_cmd != '':
+            break
+        basename = basename[:basename.rfind("_")]
+    return input_cmd
+
+
+def expand(elements, index):
+    if index < -1:
+        return ["", ""]
+    service = elements[1]
+    subcmd = elements[2]
+    method = elements[index - 1]
+    input_cmd = get_input_cmd(service, subcmd, method)
+    if input_cmd == '':
+        return ["", ""]
+    got_string = select_resources(input_cmd)
+    if got_string == '':
+        return ["", ""]
+    elements[index] = got_string
+    new_buf = " ".join(elements)
+    if index == len(elements) - 1:
+        new_pos = len(new_buf)
+    else:
+        new_pos = len(" ".join(elements[:index + 1]))
+    return [new_pos, new_buf]
 
 
 def get_cur_element_index(elements, pos):
@@ -15,18 +74,13 @@ def get_cur_element_index(elements, pos):
     return len(elements)
 
 
-def get_target_file_name(buf, pos):
-    elements = shlex.split(buf)
-    pos -= len(buf) - len(" ".join(elements))
-    cur_element_index = get_cur_element_index(elements, pos)
+buf = sys.argv[2]
+pos = int(sys.argv[1])
 
-    if cur_element_index > 0 and elements[cur_element_index].startswith(
-            '<') and elements[cur_element_index].endswith('>'):
-        service = elements[1]
-        subcmd = elements[2]
-        argument = elements[cur_element_index - 1]
-    return "{}_{}_{}".format(argument, service, subcmd)
-
-
-target_file_name = get_target_file_name(buf, pos)
-print(target_file_name)
+elements = shlex.split(buf)
+pos -= len(buf) - len(" ".join(elements))
+index = get_cur_element_index(elements, pos)
+if elements[index].startswith('<') and elements[index].endswith('>'):
+    [new_pos, new_buf] = expand(elements, index)
+    print(new_pos)
+    print(new_buf)
