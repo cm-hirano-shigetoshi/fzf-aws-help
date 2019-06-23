6a4f0c76a3679a67cf4f9eb653ead20362d574bf
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-06-23 23:24:44 +0900

    関数名とウィジェット名を区別

--- select_command/fzf-aws-help.zsh
+++ select_command/fzf-aws-help.zsh
@@ -1,8 +1,8 @@
 FZF_AWS_HELP_HOME=${0:A:h}
 export FZF_AWS_HELP_HOME
-function fzf_aws_help() {
+function __fzf_aws_help() {
   BUFFER=$(fzfer run ${FZF_AWS_HELP_HOME}/fzf-aws-help.yml)
   CURSOR=${#BUFFER}
   zle redisplay
 }
-zle -N fzf_aws_help
+zle -N fzf-aws-help __fzf_aws_help
