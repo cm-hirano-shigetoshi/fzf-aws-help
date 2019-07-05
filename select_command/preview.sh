43390d1e082700b77f39ac2941d2c687c690a3a0
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-07-05 18:46:03 +0900

    readlinkを排除

--- select_command/preview.sh
+++ select_command/preview.sh
@@ -3,7 +3,7 @@ set -eu
 
 target_cmd="aws ${1/\// }"
 readonly LOCAL_HELP_DIR="${FZF_AWS_HELP_COMMAND_HOME}/help"
-readonly LOCAL_FILE="$(readlink -m ${LOCAL_HELP_DIR}/${target_cmd// /_})"
+readonly LOCAL_FILE="${LOCAL_HELP_DIR}/${target_cmd// /_}"
 if [[ ! -e ${LOCAL_FILE} ]]; then
   mkdir -p $(dirname ${LOCAL_FILE})
   ${target_cmd} help > ${LOCAL_FILE}
